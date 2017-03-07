5f9dee7f1e58d0a3903f17885b1167f2f36c1250
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-03-07 17:08:02 +0000

    Spike refactored version of gilded_rose.rb to get the hang of the program

---
+++ ruby/lib/alt_rose.rb
@@ -0,0 +1,59 @@
+class AltRose
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_items
+    items.each { |item| item.update_quality,update_sell_in }
+  end
+
+  def update_quality
+      item.appreciation_value
+      item.quality < 50 && item.quality > 0 ? item.quality += item.check_sell_in.@multiple : item.ignore
+  end
+
+  def update_sell_in
+     @sell_in -= 1
+  end
+
+  def appreciation_value
+    if item.name == 'Aged Brie'
+      @multiple = 1
+    elsif item.name == 'Conjured'
+      @multiple = -2
+    elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      item.sell_in < 0 ? update_backstage_passes_specially : item
+    elsif item.name != 'Sulfuras, Hand of Ragnaros'
+      @multiple = -1
+    end
+  end
+
+  def check_sell_in
+    item.sell_in < 0 ? @multiple *= 2 : @multiple
+  end
+
+  def update_backstage_passes_specially
+    if item.sell_in < 11
+      item.sell_in < 6 ? @multiple = 3 : @multiple = 2
+    end
+  end
+
+  def ignore
+    item.quality == item.quality
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
---
+++ ruby/spec/alt_rose_spec.rb
@@ -0,0 +1,22 @@
+
+describe AltRose do
+  describe "update_quality" do
+
+  end
+
+  describe "update_item" do
+
+  end
+
+  describe u
+end
+
+
+
+features: check the whole bloody thing works
+
+units: check each independent bit
+  -check it categorises things properly
+  -check it returns the right multiple
+  -check it leaves the right things alone
+  -check it changes sell_in appropriately
